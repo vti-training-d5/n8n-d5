b657bbb521fd10c9272a0509632c53677027d29b
trickstar <rinnegan101220@gmail.com>
2025-04-05 17:10:33 +0700

    fix: change func isEnterpise check

--- packages/frontend/editor-ui/src/utils/rbac/checks/isEnterpriseFeatureEnabled.ts
+++ packages/frontend/editor-ui/src/utils/rbac/checks/isEnterpriseFeatureEnabled.ts
@@ -4,16 +4,17 @@ import type { RBACPermissionCheck, EnterprisePermissionOptions } from '@/types/r
 export const isEnterpriseFeatureEnabled: RBACPermissionCheck<EnterprisePermissionOptions> = (
 	options,
 ) => {
-	if (!options?.feature) {
-		return true;
-	}
+	// if (!options?.feature) {
+	// 	return true;
+	// }
 
-	const features = Array.isArray(options.feature) ? options.feature : [options.feature];
-	const settingsStore = useSettingsStore();
-	const mode = options.mode ?? 'allOf';
-	if (mode === 'allOf') {
-		return features.every((feature) => settingsStore.isEnterpriseFeatureEnabled[feature]);
-	} else {
-		return features.some((feature) => settingsStore.isEnterpriseFeatureEnabled[feature]);
-	}
+	// const features = Array.isArray(options.feature) ? options.feature : [options.feature];
+	// const settingsStore = useSettingsStore();
+	// const mode = options.mode ?? 'allOf';
+	// if (mode === 'allOf') {
+	// 	return features.every((feature) => settingsStore.isEnterpriseFeatureEnabled[feature]);
+	// } else {
+	// 	return features.some((feature) => settingsStore.isEnterpriseFeatureEnabled[feature]);
+	// }
+	return true;
 };
